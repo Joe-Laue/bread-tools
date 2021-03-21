b8a5d6d458909f9190f2779170da02e2ff63d500
Joe-Laue <joe.laue@outlook.com>
2021-03-21 09:01:05 +0800

    Add array builder

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 setup(
     name='bread-tools',
-    version='0.0.2',
+    version='0.0.4',
     description='Basic processing tools in common algorithms.',
     url='https://github.com/Joe-Laue/bread-tools',
 
@@ -16,7 +16,8 @@ setup(
     packages=['bread.tools.data',
               'bread.tools.file',
               'bread.tools.log',
-              'bread.tools.text'],
+              'bread.tools.text',
+              'bread.tools.net'],
 
     # License
     license='MIT',
